35f003adb3efc9d3511eacde4faa7d69dc30cc2b
Paul Haddad <paulh16@gmail.com>
2016-01-24 17:22:26 -0600

    Use of #fetch to set defaults * Page 49

--- managing_dependencies/gear.rb
+++ managing_dependencies/gear.rb
@@ -2,9 +2,9 @@ class Gear
   attr_reader :chainring, :cog, :wheel
 
   def initialize(args)
-    @chainring = args[:chainring] || 40 #Explicitly define defaults
-    @cog = args[:cog] || 18
-    @wheel = args[:wheel]
+    @chainring = args.fetch(:chainring, 40) # Use of fetch to set defaults
+    @cog = args.fetch(:cog, 18)
+    @wheel = args.fetch(:wheel)
   end
 
   def gear_inches
