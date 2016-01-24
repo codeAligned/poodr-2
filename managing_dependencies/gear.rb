9364aa7b81a261be932a0658b2a75729f4234fcb
Paul Haddad <paulh16@gmail.com>
2016-01-24 16:54:28 -0600

    Isolate Instance Creation - Example 2 * Lazily instantiate Wheel object inside an instance method * Page 43

--- managing_dependencies/gear.rb
+++ managing_dependencies/gear.rb
@@ -1,16 +1,19 @@
 class Gear
   attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog, wheel)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @rim = rim
+    @tire = tire
   end
 
   def gear_inches
     ratio * wheel.diameter
   end
+
+  def wheel
+    @wheel = ||= Wheel.new(rim, tire) # Dependency moved to its own instance method that is lazily evaluated.
+  end
 end
 
-# Gear expects a 'Duck' that knows 'diameter'
-puts Gear.new(52, 11, 26, 1.5).gear_inches
