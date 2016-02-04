6ec95129aaf844eea7ccc3c2909c81068ce68abe
Paul Haddad <paulh16@gmail.com>
2016-02-04 07:13:56 -0600

    Move common #size attribute to Bicycle * Page 121

--- inheritance/proper_inheritance.rb
+++ inheritance/proper_inheritance.rb
@@ -1,7 +1,18 @@
 class Bicycle
+  attr_reader :size # promoted from RoadBike
+
+  def initialize(args = {})
+    @size = args[:size] # promoted from RoadBike
+  end
 end
 
 class RoadBike < Bicycle
+  attr_reader :tape_color
+
+  def initialize(args)
+    @tape_color = args[:tape_color]
+    super(args) # RoadBike now must send super
+  end
 end
 
 class MountainBike < Bicycle
