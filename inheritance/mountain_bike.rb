b3fd7f8236de4e137e5777b9bb18af8106617f84
Paul Haddad <paulh16@gmail.com>
2016-02-03 19:54:19 -0600

    Bad use of inheritance * Having MountainBike inherit from Bicycle * Page 115

---
+++ inheritance/mountain_bike.rb
@@ -0,0 +1,40 @@
+class Bicycle
+  attr_reader :size, :tape_color
+
+  def initialize(args)
+    @size = args[:size]
+    @tape_color = args[:tape_color]
+  end
+
+  def spares
+    { chain: '10-speed',
+      tire_size: '23',
+      tape_color: tape_color
+    }
+  end
+end
+
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock, front_shock: front_shock)
+  end
+end
+
+mountain_bike = MountainBike.new(
+                  size:     'S',
+                  front_shock: 'Manitou',
+                  rear_shock: 'Fox')
+
+puts mountain_bike.size
+
+puts mountain_bike.spares
+
+
