54562b09bed3991b2b6e46a6f8b532d2f7dd7124
Paul Haddad <paulh16@gmail.com>
2016-02-04 07:33:52 -0600

    Promote #chain and #tire_size to Bicycle * Page 125

--- inheritance/proper_inheritance.rb
+++ inheritance/proper_inheritance.rb
@@ -1,8 +1,10 @@
 class Bicycle
-  attr_reader :size # promoted from RoadBike
+  attr_reader :size, :chain, :tire_size
 
   def initialize(args = {})
-    @size = args[:size] # promoted from RoadBike
+    @size      = args[:size]
+    @chain     = args[:chain]
+    @tire_size = args[:tire_size]
   end
 end
 
