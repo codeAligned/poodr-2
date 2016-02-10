aca34d18ca05d29ea390ff3d9917792fa447d13e
Paul Haddad <paulh16@gmail.com>
2016-02-10 07:30:48 -0600

    Extract abstraction to a module * Remove scharable code from Bicycle * Page 150

--- inheritance/modules.rb
+++ inheritance/modules.rb
@@ -6,30 +6,35 @@ class Schedule
   end
 end
 
-class Bicycle
-  attr_reader :schedule, :size, :chain, :tire_size
+module Schedulable
+  attr_writer :schedule
 
-  # Inject the Schedule and provide a default
-  def initialize(args = {})
-    @schedule = args[:schedule] || Schedule.new
-    # ....
+  def schedule
+    @schedule ||= ::Schedule.new
   end
 
-  # Return true if this bicycle is available during this (now Bicycle
-  # specific) interval.
   def schedulable?(start_date, end_date)
     !scheduled?(start_date - lead_days, end_date)
   end
 
-  # Return the schedule's answer
   def scheduled?(start_date, end_date)
     schedule.scheduled?(self, start_date, end_date)
   end
 
-  # Return the number of lead_days before a bicycle can be scheduled.
+  # includers may override
+  def lead_days
+    0
+  end
+end
+
+class Bicycle
+  include Schedulable
+
   def lead_days
     1
   end
+
+  # ....
 end
 
 require "date"
