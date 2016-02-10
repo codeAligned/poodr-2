22d9d5c21e67dd348bccaa2215aa1811e587c201
Paul Haddad <paulh16@gmail.com>
2016-02-10 07:34:55 -0600

    Create additional classes that behave like a shareable * Vehicle and Mechanic * Page 152

--- inheritance/modules.rb
+++ inheritance/modules.rb
@@ -37,9 +37,31 @@ class Bicycle
   # ....
 end
 
+class Vehicle
+  include Schedulable
+
+  def lead_days
+    3
+  end
+end
+
+class Mechanic
+  include Schedulable
+
+  def lead_days
+    4
+  end
+end
+
 require "date"
 starting = Date.parse("2015/09/04")
 ending = Date.parse("2015/09/10")
 
 b = Bicycle.new
 b.schedulable?(starting, ending)
+
+v = Vehicle.new
+v.schedulable?(starting, ending)
+
+m = Mechanic.new
+m.schedulable?(starting, ending)
