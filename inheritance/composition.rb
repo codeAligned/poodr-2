df1583a2ff591601caadaacce45422d85c9a4af5
Paul Haddad <paulh16@gmail.com>
2016-02-11 07:27:34 -0600

    Create a Parts hierarchy * Page 165-167

--- inheritance/composition.rb
+++ inheritance/composition.rb
@@ -11,7 +11,39 @@ class Bicycle
   end
 end
 
-class RoadBike < Bicycle
+class Parts
+  attr_reader :chain, :tire_size
+
+  def initialize(args = {})
+    @chain     = args[:chain]     || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+    post_initialize(args)
+  end
+
+  def spares
+    { tire_size: tire_size,
+      chain:     chain}.merge(local_spares)
+  end
+
+  def default_tire_size
+    raise NotImplementdError
+  end
+
+  # subclasses may override
+  def post_initialize(args)
+    nil
+  end
+
+  def local_spares
+    {}
+  end
+
+  def default_chain
+    '10-speed'
+  end
+end
+
+class RoadBikeParts < Parts
   attr_reader :tape_color
 
   def post_initialize(args)
@@ -27,12 +59,12 @@ class RoadBike < Bicycle
   end
 end
 
-class MountainBike < Bicycle
+class MountainBikeParts < Parts
   attr_reader :front_shock, :rear_shock
 
-  def initialize(args)
-    @front_shock = front_shock
-    @rear_shock  = rear_shock
+  def post_initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
   end
 
   def local_spares
@@ -42,41 +74,20 @@ class MountainBike < Bicycle
   def default_tire_size # subclass default
     '2.1'
   end
-
-  def spares
-    super.merge({ rear_shock: rear_shock })
-  end
 end
 
-class RecumbantBike < Bicycle
-  attr_reader :flag
+road_bike = 
+  Bicycle.new(
+    size: "L",
+    parts: RoadBikeParts.new(tape_color: "red"))
 
-  def post_initialize(args)
-    @flag = args[:flag]
-  end
+puts road_bike.size
+puts road_bike.spares
 
-  def local_spares
-    { flag: flag }
-  end
+mountain_bike =
+  Bicycle.new(
+    size: "L",
+    parts: MountainBikeParts.new(rear_shock: "Fox"))
 
-  def default_chain
-    '9-speed'
-  end
-
-  def default_tire_size
-    '28'
-  end
-end
-
-road_bike = RoadBike.new(size: 'M', tape_color: 'red')
-
-puts road_bike.tire_size
-puts road_bike.chain
-
-mountain_bike = MountainBike.new(size: 'S', front_shock: 'Manitou', rear_shock: 'Fox')
-
-puts mountain_bike.tire_size
-puts mountain_bike.chain
-
-recumbant_bike = RecumbantBike.new(flag: 'tall and orange')
-puts recumbant_bike.spares 
+puts mountain_bike.size
+puts mountain_bike.spares
