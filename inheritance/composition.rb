7280ca645ce5945abce41f43212cda0246808567
Paul Haddad <paulh16@gmail.com>
2016-02-11 20:45:05 -0600

    Final composition example * Pages 181-182

--- inheritance/composition.rb
+++ inheritance/composition.rb
@@ -26,82 +26,44 @@ class Parts
   end
 end
 
-class Part
-  attr_reader :name, :description, :needs_spare
-
-  def initialize(args)
-    @name = args[:name]
-    @description = args[:description]
-    @needs_spare = args.fetch(:needs_spare, true)
-  end
-end
-
+require 'ostruct'
 module PartsFactory
-  def self.build(config, part_class = Part, parts_class = Parts)
+  def self.build(config, parts_class = Parts)
     parts_class.new(
       config.map do |part_config|
-        part_class.new(
-          name: part_config[0],
-          description: part_config[1],
-          needs_spare: part_config.fetch(2, true))
+        create_part(part_config)
       end
     )
   end
-end
 
-class RoadBikeParts < Parts
-  attr_reader :tape_color
-
-  def post_initialize(args)
-    @tape_color = args[:tape_color] # RoadBike can optionally override it
-  end
-
-  def local_spares
-    { tape_color: tape_color }
-  end
-
-  def default_tire_size # subclass default
-    '23'
+  def self.create_part(part_config)
+    OpenStruct.new(
+      name:        part_config[0],
+      description: part_config[1],
+      needs_spare: part_config.fetch(2, true))
   end
 end
 
-class MountainBikeParts < Parts
-  attr_reader :front_shock, :rear_shock
+road_config =
+  [["chain",      "10-speed"],
+   ["tire_size",  "23"],
+   ["tape_color", "red"]]
 
-  def post_initialize(args)
-    @front_shock = args[:front_shock]
-    @rear_shock  = args[:rear_shock]
-  end
+mountain_config =
+  [["chain",      "10-speed"],
+   ["tire_size",  "2.1"],
+   ["front_shock", "Manitou", false],
+   ["rear_shock",  "Fox"]]
 
-  def local_spares
-    { rear_shock: rear_shock }
-  end
-
-  def default_tire_size # subclass default
-    '2.1'
-  end
-end
-
-chain = Part.new(name: "chain", description: "10-speed")
-
-road_tire = Part.new(name: "tire_size", description: "23")
-
-tape = Part.new(name: "tape_color", description: "red")
-
-mountain_tire = Part.new(name: "tire_size", description: "2.1")
-
-rear_shock = Part.new(name: "rear_shock", description: "Fox")
-
-front_shock = Part.new(name: "front_shock", description: "Manitou", needs_spare: false)
-
-road_bike_parts = Parts.new([chain, road_tire, tape])
-
-mountain_bike_parts = Parts.new([chain, mountain_tire, front_shock, rear_shock])
+recumbent_config =
+  [["chain",      "9-speed"],
+   ["tire_size",  "28"],
+   ["flag", "tall and orange"]]
 
 road_bike = 
   Bicycle.new(
     size: "L",
-    parts: road_bike_parts)
+    parts: PartsFactory.build(road_config))
 
 puts road_bike.size
 puts road_bike.spares
@@ -109,9 +71,15 @@ puts road_bike.spares
 mountain_bike =
   Bicycle.new(
     size: "L",
-    parts: mountain_bike_parts)
+    parts: PartsFactory.build(mountain_config))
 
 puts mountain_bike.size
 puts mountain_bike.spares
-puts mountain_bike.spares.size
-puts mountain_bike.parts.size
+
+recumbent_bike = 
+  Bicycle.new(
+    size: "L",
+    parts: PartsFactory.build(recumbent_config))
+
+puts recumbent_bike.size
+puts recumbent_bike.spares
