45c6f86a6d3ae49d8e20e7adbee401a6ed49cd96
Paul Haddad <paulh16@gmail.com>
2016-02-14 14:48:19 -0600

    Using tests to document roles * Add a test to document the diameterizable interface. * This is not a satisfactory solution.and a better one will be presented * later.

--- wheel_test.rb
+++ wheel_test.rb
@@ -2,6 +2,14 @@ require "./testing_incoming_messages"
 require "minitest"
 
 class WheelTest < Minitest::Test
+  def setup
+    @wheel = Wheelnew(26, 1.5)
+  end
+
+  def test_implements_the_diameterizable_interface
+    assert_respond_to(@wheel, :diameter)
+  end
+
   def test_calculates_diameter
     wheel = Wheel.new(26, 1.5)
 
