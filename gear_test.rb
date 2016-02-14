6472bd37dcbd21e4046d37e9d349398754b5351b
Paul Haddad <paulh16@gmail.com>
2016-02-14 14:38:51 -0600

    Creating test doubles * Use a test double instead of a Wheel object in the GearTest. * Page 210

--- gear_test.rb
+++ gear_test.rb
@@ -1,9 +1,16 @@
+# Create a player of the "Diameterizable" role
+class DiameterDouble
+  def diameter
+    10
+  end
+end
+
 class GearTest
   def test_calculates_gear_inches
     gear = Gear.new(
             chainring: 52,
             cog: 11,
-            wheel: Wheel.new(26, 1.5))
+            wheel: DiameterDouble.new)
 
     assert_in_delta(137.1, gear.gear_inches, 0.01)
   end
