4d8360901c720f2529c655b29490096289c08872
Paul Haddad <paulh16@gmail.com>
2016-02-15 08:10:33 -0600

    Second step of refactoring * Change trip to use Preparer interface

--- testing_duck_types.rb
+++ testing_duck_types.rb
@@ -22,17 +22,7 @@ class Trip
   attr_reader :bicycles, :customers, :vehicle
 
   def prepare(preparers)
-    preparers.each do |preparer|
-      case preparer
-      when Mechanic
-        preparer.prepare_bicycles
-      when TripCoordinator
-        preparer.buy_food(customers)
-      when Driver
-        preparer.gas_up(vehicle)
-        preparer.fill_water_tank(vehicle)
-      end
-    end
+    preparers.each { |preparer| preparer.prepare_trip(self) }
   end
 end
 
