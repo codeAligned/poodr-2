9faf8aa8026e8bfec3398cea428e0242a724b892
Paul Haddad <paulh16@gmail.com>
2016-02-16 07:50:27 -0600

    Confirming subclass behavior * Page 233

--- testing_inherited_code_test.rb
+++ testing_inherited_code_test.rb
@@ -34,8 +34,32 @@ end
 
 class RoadBikeTest < MiniTest::Unit::TestCase
   include BicycleInterfaceTest
+  include BicycleSubclassTest
 
   def setup
     @bike = @object = Roadbike.new
+  end
+end
+
+class MountainBikeTest < MiniTest::Unit::TestCase
+  include BicycleInterfaceTest
+  include BicycleSubclassTest
+
+  def setup
+    @bike = @object = MountainBike.new
+  end
+end
+
+module BicycleSubclassTest
+  def test_responds_to_post_initialize
+    assert_respond_to(@object, :post_initialize)
+  end
+
+  def test_responds_to_local_spares
+    assert_respond_to(@object, :local_spares)
+  end
+
+  def test_responds_to_default_tire_size
+    assert_respond_to(@object, :default_tire_size)
   end
 end
