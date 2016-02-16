daa3193702fd2ddf22ff694de42d24a443575c7c
Paul Haddad <paulh16@gmail.com>
2016-02-16 07:45:19 -0600

    Testing the inherited interface * Pages 229-232

---
+++ testing_inherited_code_test.rb
@@ -0,0 +1,41 @@
+module BicycleInterfaceTest
+  def test_responds_to_default_tire_size
+    assert_respond_to(@object, :default_tire_size)
+  end
+
+  def test_responds_to_default_chain
+    assert_respond_to(@object, :default_chain)
+  end
+
+  def test_responds_to_chain
+    assert_respond_to(@object, :chain)
+  end
+
+  def test_responds_to_size
+    assert_respond_to(@object, :size)
+  end
+
+  def test_responds_to_tire_size
+    assert_respond_to(@object, :tire_size)
+  end
+
+  def test_responds_to_spares
+    assert_respond_to(@object, :spares)
+  end
+end
+
+class BicycleTest < MiniTest::Unit::TestCase
+  include BicycleInterfaceTest
+
+  def setup
+    @bike = @object = Bicycle.new( { tire_size: 0 } )
+  end
+end
+
+class RoadBikeTest < MiniTest::Unit::TestCase
+  include BicycleInterfaceTest
+
+  def setup
+    @bike = @object = Roadbike.new
+  end
+end
